9d9e5b063a640d797ce13d3cbbf10d94e5e3ce26
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-14 16:37:00 +0530

    allow all origin

--- ocr_backend/settings.py
+++ ocr_backend/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 ALLOWED_HOSTS = ["*"] 
-CORS_ALLOWED_ORIGINS = [
-    "https://food-pharma.netlify.app",
-    "http://localhost:5173",   # local dev
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://food-pharma.netlify.app",
+#     "http://localhost:5173",   # local dev
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
