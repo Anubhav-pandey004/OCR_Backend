0dff4004759772d37958060356ee7bf838b4a2b8
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-14 17:14:23 +0530

    Update settings.py

--- ocr_backend/settings.py
+++ ocr_backend/settings.py
@@ -57,30 +57,33 @@ MIDDLEWARE = [
 #     "https://food-pharma.netlify.app",
 #     "http://localhost:5173",  # for local dev
 # ]
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "https://food-pharma.netlify.app",
+]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_METHODS = ["*"]
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
+# CORS_ALLOW_HEADERS = [
+#     "accept",
+#     "accept-encoding",
+#     "authorization",
+#     "content-type",
+#     "dnt",
+#     "origin",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+# ]
 
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
+# CORS_ALLOW_METHODS = [
+#     "DELETE",
+#     "GET",
+#     "OPTIONS",
+#     "PATCH",
+#     "POST",
+#     "PUT",
+# ]
 
 
 # CORS_ALLOWED_ORIGINS = [
