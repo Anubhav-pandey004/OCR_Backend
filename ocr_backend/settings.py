45bc05324a4f439f7ac06964cad80cdd1b337a4b
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-14 19:09:00 +0530

    Update settings.py

--- ocr_backend/settings.py
+++ ocr_backend/settings.py
@@ -57,12 +57,14 @@ MIDDLEWARE = [
 #     "https://food-pharma.netlify.app",
 #     "http://localhost:5173",  # for local dev
 # ]
-CORS_ALLOWED_ORIGINS = [
-    "https://food-pharma.netlify.app",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://food-pharma.netlify.app",
+# ] 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
 CORS_ALLOW_METHODS = ["*"]
+
 
 # CORS_ALLOW_HEADERS = [
 #     "accept",
